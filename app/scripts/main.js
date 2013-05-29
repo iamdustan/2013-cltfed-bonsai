b018e345138614287789527724c087634f1bc7b2
Dustan Kasten <dustan.kasten@gmail.com>
2013-05-29 13:23:42 -0400

    Random commit of a bunch of stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 require.config({
   paths: {
     jquery: '../components/jquery/jquery',
-    bonsai: './vendor/bonsai'
+    bonsai: './vendor/bonsai',
+    //bonsai: './vendor/bonsai.node'
   },
   shim: {
     bonsai: {
@@ -11,14 +12,38 @@ require.config({
   }
 });
 
-require(['app', 'jquery', 'bonsai'], function (app, $, bonsai) {
-    'use strict';
-    // use app here
-    console.log('Running jQuery %s', $().jquery);
-    console.log('Running Bonsai %s', bonsai.version);
+//require(['starField', 'jquery', 'bonsai'], function (app, $, bonsai) {
+//require(['breakout', 'jquery', 'bonsai'], function (app, $, bonsai) {
+//require(['cube', 'jquery', 'bonsai'], function (app, $, bonsai) {
+require(['nodeRunner', 'jquery', 'bonsai'], function (app, $, bonsai) {
+  'use strict';
+  // use app here
+  console.log('Running jQuery %s', $().jquery);
+  console.log('Running Bonsai %s', bonsai.version);
+  var stage;
 
-    bonsai.run('movie', {
-      code: app
-    });
+  if (app.runnerContext) {
+    stage = bonsai
+      .setup(app)
+      .run(document.getElementById('movie'), {
+        width: 500,
+        height: 500,
+        framerate: 100
+      });
 
+    app.start(stage);
+  }
+  else {
+    stage = bonsai
+      .setup({ runnerContext: bonsai.IframeRunnerContext })
+      .run('movie', {
+        width: window.outerWidth,
+        height: window.outerHeight,
+        code: app,
+        framerate: 50
+      });
+  }
+
+  window.stage = stage;
+  return stage;
 });
